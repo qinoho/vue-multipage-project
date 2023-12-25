18cba62d22e3fe4a33ac41ca2a4c7b230ef5dc36
qinoho <qinpengshuai@petem.com.cn>
2023-12-25 16:46:42 +0800

    feat(rem): px auto converted rem

--- src/views/proj/App.vue
+++ src/views/proj/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
-    <h1>测试组件能否正常使用</h1>
+    <h1 class="divd">test</h1>
     <MyContent />
+    <HelloWorld />
   </div>
 </template>
 
 <script setup>
 import MyContent from './cpns/contnet.vue'
-
-console.log('123312312312============')
+// import HelloWorld from '../../components/HelloWorld.vue.js'
+import HelloWorld from '@/components/HelloWorld.vue'
+// console.log('123312312312============')
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.divd {
+  transform: rotate(30deg);
+  display: block;
+}
+</style>
--- src/views/proj/main.ts
+++ src/views/proj/main.ts
@@ -1,12 +1,9 @@
 // import './assets/main.css'
-
-console.log('first')
 import { createApp } from 'vue'
 // // import { createPinia } from 'pinia'
-
 import App from './App.vue'
 // // import router from './router'
-
+import '@/lib/rem.js'
 const app = createApp(App)
 
 // // app.use(createPinia())
