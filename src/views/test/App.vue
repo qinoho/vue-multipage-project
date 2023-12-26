15cc0a27a4df26ad9aa6948ca2e437857912d308
qinoho <qinpengshuai@petem.com.cn>
2023-12-26 10:36:37 +0800

    test(pinia): test pinia run

--- src/views/test/App.vue
+++ src/views/test/App.vue
@@ -1,20 +1,22 @@
 <template>
   <div>
     <h1>哈哈哈哈哈哈或</h1>
+    <h2>{{ counter.count }}</h2>
+    <h2>{{ counter.doubleCount }}</h2>
+    <button @click="counter.increment">add</button>
   </div>
 </template>
 
 <script>
+import { useCounterStore } from './stores'
 export default {
-  setup () {
-    
+  setup() {
+    const counter = useCounterStore()
     console.log('123312312312============')
 
-    return {}
-  }
+    return { counter }
+  },
 }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/views/test/main.ts
+++ src/views/test/main.ts
@@ -2,14 +2,14 @@
 
 console.log('first')
 import { createApp } from 'vue'
-// // import { createPinia } from 'pinia'
+import { createPinia } from 'pinia'
 
 import App from './App.vue'
 // // import router from './router'
 
 const app = createApp(App)
 
-// // app.use(createPinia())
+app.use(createPinia())
 // // app.use(router)
 
 app.mount('#app')
